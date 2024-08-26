ba48bdeeb2754f58b031f49702a058ec3d303a15
IslamAAli <islam.a.mustafa@gmail.com>
2024-08-26 15:14:17 -0600

    adding new lines library

--- build.sh
+++ build.sh
@@ -6,8 +6,11 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
-cd ../../g2o
+cd ../../EDLines
+echo "Configuring and building Thirdparty/EDLines ..."
+make
 
+cd ../g2o
 echo "Configuring and building Thirdparty/g2o ..."
 
 mkdir build
