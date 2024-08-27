29953de8134d3b3826bde2713f1e315e4d39c635
IslamAAli <islam.a.mustafa@gmail.com>
2024-08-27 14:10:45 -0600

    adding new copy constructor

--- src/Modeler/converters.cpp
+++ src/Modeler/converters.cpp
@@ -2,11 +2,12 @@
 #define __CONVERTERS_CPP
 
 #include "Modeler/converters.h"
+#include <iostream>
 
 namespace CARV_HELPERS
 {
     cv::Mat se3ToCvMat(const Sophus::SE3<float>& se3) {
-        cout << "Called se3ToCvMat " << endl;
+        std::cout << "Called se3ToCvMat " << std::endl;
         // Extract rotation matrix and translation vector
         Eigen::Matrix3f R = se3.rotationMatrix();
         Eigen::Vector3f t = se3.translation();
@@ -25,12 +26,12 @@ namespace CARV_HELPERS
         for (int i = 0; i < 3; ++i) {
             cvTransform.at<float>(i, 3) = t(i);
         }
-        cout << "Return se3ToCvMat : " << cvTransform << endl;
+        std::cout << "Return se3ToCvMat : " << cvTransform << std::endl;
         return cvTransform;
     }
 
     cv::Mat vector3fToCvMat(const Eigen::Vector3f& vector) {
-        cout << "Called vector3fToCvMat " << endl;
+        std::cout << "Called vector3fToCvMat " << std::endl;
         // Create cv::Mat with 1 row, 3 columns, and CV_32F type
         cv::Mat cvMat(1, 3, CV_32F);
 
@@ -39,12 +40,12 @@ namespace CARV_HELPERS
         cvMat.at<float>(0, 1) = vector(1);
         cvMat.at<float>(0, 2) = vector(2);
 
-        cout << "Return vector3fToCvMat : " << cvMat << endl;
+        std::cout << "Return vector3fToCvMat : " << cvMat << std::endl;
         return cvMat;
     }
 
     Eigen::Vector3f matToEigenVector3f(const cv::Mat& mat) {
-        cout << "Called matToEigenVector3f " << endl;
+        std::cout << "Called matToEigenVector3f " << std::endl;
         Eigen::Vector3f eigenVec;
 
         if (mat.rows == 3 && mat.cols == 1 && mat.type() == CV_32F) {
@@ -55,7 +56,7 @@ namespace CARV_HELPERS
             throw std::invalid_argument("Input cv::Mat is not a 3x1 matrix of type CV_32F");
         }
 
-        cout << "Return matToEigenVector3f : " << cvMat << endl;
+        std::cout << "Return matToEigenVector3f : " << cvMat << std::endl;
         return eigenVec;
     }
 }
